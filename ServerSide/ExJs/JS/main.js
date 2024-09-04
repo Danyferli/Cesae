1b45e7c0ed6282955cbe6fa2845c76e4f8f160f5
Daniele Lima <106557595+Danyferli@users.noreply.github.com>
2024-09-04 17:57:41 +0100

    Aula 4/9

---
+++ ServerSide/ExJs/JS/main.js
@@ -0,0 +1,111 @@
+// let myName = "Daniele";
+// let myLuckyNumber = 5;
+// alert(myName+ '' + myLuckyNumber);
+//const MESSAGE =  '  TASTE THE RAINBOW!  '
+
+//let whisper = MESSAGE .trim().toLowerCase();
+
+//console.log(whisper);
+
+ 
+//const WORD = 'Skateboard'
+ 
+//let facialHair = WORD.slice (5,10);
+//facialHair=facialHair.replace('o', 'e')
+//alert(facialHair)
+
+//let nymeName = prompt('Qual é seu nome');
+
+
+//let dayOfWeek = prompt ('Que dia da semana é'). toLowerCase();
+
+//switch (dayOfWeek){
+    //case 'segunda':
+    //alert(mais uma dia');
+    //break;
+   // case 'terça':
+   // alert (mais uma dia na semana');
+    //break;
+    //case 'sexta':
+   // alert ('sobrevivemos a mais uma semana');
+    ///break;
+ //} 
+//let pw = prompt('qual é a pass');
+//if(pw.length <5){
+    //alert('ok');
+//else{
+  //alert()
+  
+
+//}
+//let series = ['Friends', 'Dark', 'Breaking Bad', 'House of Dragons', 'The Bear', 'Station 11'];
+
+//series[1] = 'DragonBall';
+//series.push('GOT');
+//series.unshift('Halt and Catch Fire');
+
+//console.log(series);
+
+
+//let planets = ['Mercúrio', 'Vénus', 'Terra', 'Mart', 'Jupiter', 'Saturno', 'Urano', 'Neptuno', 'Plutão'];
+
+//let planets[3] = 'Marte';
+//planets.pop( );
+//planets.unshift('Yaap');
+
+//console.log(planets);
+//let courses=[
+    //  ['Apis', 'Dom']
+      //['CSS','Html']
+      //'Algoritmia',
+     // true,
+     // 16,
+       //'Android'
+    //]; 
+
+  //console.log(courses [5])
+   // const airplaneSeats = [
+      //  ['Ruth', 'Anthony', 'Stevie'],
+      //  ['Amelia', 'Pedro', 'Maya'],
+       // ['Xavier', 'Ananya', 'Luis'],
+       // ['Luke', null, 'Deniz'],
+       // ['Rin', 'Sakura', 'Francisco']
+    ////]; 
+//airplaneSeats [3][1] = 'Daniele'
+//console.log(airplaneSeats)
+///let JS = {
+   // avaliação : ['teste', 'exame'],
+    //formador : "Sara monteiro",
+    //sala : 6,
+    //horas : 50,
+    //ferias : true,
+//}
+ 
+//console.log("A avaliação é " + JS.avaliação);
+//console.log("A formadora é " + JS.formador);
+
+//let objecto = {
+//name:'faca',
+//inStock: true ,
+//Price:1.99 , 
+//Colors:['vermelho',  'azul', 'verde' ]
+//}
+
+//console.log(product.price);
+//console.log(product.colors[2])
+//product.price = 2.55;
+//console.log(product.price);
+//let planets = ['Mercúrio', 'Vénus', 'Terra', 'Mart', 'Jupiter', 'Saturno', 'Urano', 'Neptuno', 'Plutão'];
+
+//for(let i=0; i<movies.length; i++)[
+  //console.log('na posição'+i+movies[i])
+//]
+
+//ex letra maiuscula
+//  const people = ["Scooby", "Velma", "Daphne", "Shaggy", "Fred"];
+//  for(let i=0; i<people.length; i++)
+//    {
+//   console.log(people[i].toUpperCase())
+  
+ 
+//  }
